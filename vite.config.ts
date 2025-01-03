055d1ff02fd8c61d1b4d0e006e3749a3695b3123
yosbany <ytejas.86@gmail.com>
2025-01-02 23:22:50 -0300

    Actualización del Proyecto

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,13 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   base: '/production/',
+  build: {
+    outDir: 'dist',
+    sourcemap: true
+  },
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
